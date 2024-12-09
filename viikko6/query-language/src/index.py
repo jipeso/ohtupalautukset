c836106d0192e026f0b2a1c4aed4be8846cdda43
jipeso <jipesonen@gmail.com>
2024-12-09 15:02:22 +0200

    viikko6 t1-3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -8,14 +8,16 @@ def main():
     stats = Statistics(reader)
 
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
     )
+)
 
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,32 @@ class And:
 
         return True
 
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return False
+
+        return True
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+class All:
+    def test(self, player):
+        if player:
+            return True
 
 class PlaysIn:
     def __init__(self, team):
@@ -27,3 +53,13 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
